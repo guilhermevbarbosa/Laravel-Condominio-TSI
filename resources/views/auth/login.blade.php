9432bf3c5c7715f8758a65cdc066e1a0c80c7f8a
Guilherme Vinicius Barbosa <guilherme.barbosa@fri.to>
2020-10-27 23:44:30 -0300

    init e logica de login Facebook e Github

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,18 @@
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <x-jet-validation-errors class="mb-4" />
+
+        <div>
+            <a href="{{route('social.login', ['provider'=>'github'])}}">Login Github</a>
+        </div>
+
+        <div>
+            <a href="{{route('social.login', ['provider'=>'facebook'])}}">Login Facebook</a>
+        </div>
+
+    </x-jet-authentication-card>
+</x-guest-layout>
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('login/{provider}', [LoginController::class, 'redirectToProvider'])->name('social.login');
+Route::get('login/{provider}/callback', [LoginController::class, 'handleProviderCallback'])->name('social.callback');
